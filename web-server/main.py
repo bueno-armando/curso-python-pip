f66650cd5f4005d38c38bad844b48941f70c6b55
Armando Bueno <armandoseb.bueno@gmail.com>
2023-03-29 21:10:31 -0600

    Update web-server project

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+#instancia de fastapi
+app = FastAPI()
+
+# declarador
+@app.get('/')
+def get_list():
+    return [1,2,3]
+# ahora estamos devolviendo una variable, lo apropiado
+# seria devolver documentos HTML
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Mi primera página en un servidor :)</h1>
+        <p>Un paragraph</p>
+    """
 
 def run():
     store.get_categories()
